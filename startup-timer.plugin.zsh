d842689d6965777e05c8e1154a02152d16352384
p1r473 <subwayjared@gmail.com>
2024-05-21 21:52:06 -0400

    Update startup-timer.plugin.zsh

--- startup-timer.plugin.zsh
+++ startup-timer.plugin.zsh
@@ -1,1 +1,12 @@
-init.zsh
+seconds_to_milliseconds() {
+    if [[ -z $1 ]]; then
+        echo "Usage: seconds_to_milliseconds <seconds>"
+        return 1
+    fi
+    local seconds=$1
+    local milliseconds=$(echo "$seconds * 1000" | bc)
+    echo "$seconds seconds is equal to $milliseconds milliseconds."
+}
+
+typeset -F SECONDS
+seconds_to_milliseconds $SECONDS
